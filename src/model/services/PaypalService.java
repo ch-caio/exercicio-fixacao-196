8c81f3e4e97757f060d89d01915d0b32113e5706
Caio Pereira <ch.caio@hotmail.com>
2021-03-22 21:15:31 -0300

    criação do projeto

---
+++ src/model/services/PaypalService.java
@@ -0,0 +1,19 @@
+package model.services;
+
+public class PaypalService implements OnlinePaymentService {
+
+	private static final Double PAYMENT_FEE = 0.02;
+	private static final Double MONTH_INTEREST = 0.01;
+	
+	@Override
+	public Double paymentFee(Double amount) {
+				return amount * PAYMENT_FEE;
+	}
+
+	@Override
+	public Double interest(Double amount, Integer months) {
+				return amount * months * MONTH_INTEREST;
+	}
+
+
+}
